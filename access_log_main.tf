d16ec7b8fdf22c2afdba7081c2d2f038903dfa6e
Marvin Sankar <22451501+Madchyle@users.noreply.github.com>
2025-08-14 11:14:30 -0500

    access_log_main.tf

--- access_log_main.tf
+++ access_log_main.tf
@@ -1,14 +1,93 @@
-provider "aws" {
-  region = "us-east-1" 
+variable "aws_region" {
+  description = "Region to deploy the resources"
+  type        = string
+  default     = "us-east-1"
 }
 
+variable "bucket_prefix" {
+  description = "Prefix for the S3 bucket name"
+  type        = string
+  default     = "aws-access-logs"
+}
+
+variable "transition_days" {
+  description = "Days before logs transition to Glacier"
+  type        = number
+  default     = 30
+}
+
+variable "expiration_days" {
+  description = "Days before logs are deleted"
+  type        = number
+  default     = 365
+}
+
+locals {
+  log_delivery_accounts = {
+    "us-east-1"      = "127311923021"
+    "us-east-2"      = "033677994240"
+    "us-west-1"      = "173754725891"
+    "us-west-2"      = "234567890123"
+    "eu-west-1"      = "156460612806"
+    "eu-west-2"      = "652711504416"
+    "eu-central-1"   = "054676820928"
+  }
+
+  unique_bucket_name = "${var.bucket_prefix}-${data.aws_caller_identity.current.account_id}-${var.aws_region}"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "access_logs" {
-  bucket = "aws-access-logs-bucket-12345" 
+  bucket = local.unique_bucket_name
+
+  tags = {
+    Name              = local.unique_bucket_name
+    prevent-recursion = "true"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "access_logs_versioning" {
+  bucket = aws_s3_bucket.access_logs.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_acl" "access_logs_acl" {
   bucket = aws_s3_bucket.access_logs.id
   acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_policy" "access_logs_policy" {
+  bucket = aws_s3_bucket.access_logs.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "S3ServerAccessLogsPolicy",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${local.log_delivery_accounts[var.aws_region]}:root"
+      },
+      "Action": "s3:PutObject",
+      "Resource": "${aws_s3_bucket.access_logs.arn}/*",
+      "Condition": {
+        "StringEquals": {
+          "s3:x-amz-acl": "bucket-owner-full-control"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
 
 resource "aws_s3_bucket_public_access_block" "access_logs_block" {
@@ -26,14 +105,14 @@ resource "aws_s3_bucket_lifecycle_configuration" "access_logs_lifecycle" {
     status = "Enabled"
 
     transition {
-      days          = 30
-      storage_class = "GLACIER" 
+      days          = var.transition_days
+      storage_class = "GLACIER"
     }
 
     expiration {
-      days = 365 
+      days = var.expiration_days
     }
-    
+
     filter {
       prefix = ""
     }
