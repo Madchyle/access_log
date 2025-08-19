6ba55546cf57eb8e676441203979d203fc9f1fa5
Marvin Sankar <22451501+Madchyle@users.noreply.github.com>
2025-08-19 10:17:12 -0500

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,1 +1,13 @@
+resource "aws_kms_key" "service_keys" {
+  for_each = local.enabled_services
+
+  description             = "CMK for ${each.key} encryption"
+  enable_key_rotation     = true
+  deletion_window_in_days = 30
+
+  tags = {
+    Name    = "${var.prefix}-${each.key}-kms"
+    Service = each.key
+  }
+}
 
