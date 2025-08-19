82bdec0410b4d2d18c26d081d6f7939c9526b173
Marvin Sankar <22451501+Madchyle@users.noreply.github.com>
2025-08-19 10:25:56 -0500

    main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -6,8 +6,17 @@ resource "aws_kms_key" "service_keys" {
   deletion_window_in_days = 30
 
   tags = {
-    Name    = "${var.prefix}-${each.key}-kms"
+    Name    = "${var.kms_services.prefix}-${each.key}-kms"
     Service = each.key
   }
 }
 
+output "kms_key_arns" {
+  description = "ARNs of created KMS keys"
+  value       = { for svc, key in aws_kms_key.service_keys : svc => key.arn }
+}
+
+output "kms_key_ids" {
+  description = "IDs of created KMS keys"
+  value       = { for svc, key in aws_kms_key.service_keys : svc => key.key_id }
+}
